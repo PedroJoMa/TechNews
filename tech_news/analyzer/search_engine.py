136d44c7b8cc5b362b4b7b204af6ba4e3a54322a
PedroJoMa <pejomadejefi@gmail.com>
2023-05-22 12:17:51 -0300

    feat: busca noticias por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from tech_news.database import db
 import re
+from datetime import datetime
 
 
 # Requisito 7
@@ -11,7 +12,22 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # regex_pattern = r"\d{4}-\d{2}-\d{2}"
+    # if not re.match(regex_pattern, date):
+    #     ValueError("Data inválida")
+    # formated_date = f"{date[8:10]}/{date[5:7]}/{date[0:4]}"
+    # response = db.news.find({
+    #     "timestamp": formated_date
+    # })
+    # return [(notice["title"], notice["url"])for notice in response]
+    try:
+        convert_date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        response = db.news.find({
+            "timestamp": convert_date
+        })
+        return [(notice["title"], notice["url"])for notice in response]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
